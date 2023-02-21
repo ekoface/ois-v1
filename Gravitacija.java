7202d21317ec37dc03d4d88c3eac70e2973731cf
Puhadrator <ziga.cesen@gmail.com>
2023-02-21 23:06:38 +0100

    Upam da bo vredu, Lp Np

---
+++ Gravitacija.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.Math;
+
+public class Gravitacija {
+
+    public static double Izracun (int visina) {
+        Scanner sc = new Scanner(System.in);
+        double v = sc.nextDouble();
+        double c = 6.674 * Math.pow(10, -11);
+        double m = 5.972 * Math.pow(10, 24);
+        double r = 6.371 * Math.pow(10, 6);
+
+        double rezultat = (c * m) / ((r + v) * (r + v));
+
+        return rezultat;
+
+    }
+
+
+    public static void main(String[] args) {
+
+        //testis
+        
+    }
+}
