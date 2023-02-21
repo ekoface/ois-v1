7eafa6f77b232e88f0363de5773ec3a3ca33a637
MrTenTim <114308680+MrTenTim@users.noreply.github.com>
2023-02-21 22:55:52 +0100

    Metoda za izpis

---
+++ Gravitacija.java
@@ -0,0 +1,5 @@
+public class Gravitacija{
+    public static String izpisi(double visina, double gravPospesek){
+        System.out.printf("Na visini %f, je gravitacijski pospesek %f %n", visina, gravPospesek);
+    }
+}
